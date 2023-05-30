b6f540710fbb93a228d8dd8d060122e1bb725853
TruongThaiBao <infocheck0808@gmail.com>
2023-05-31 01:55:22 +0700

    1st

---
+++ src/main/java/com/example/java1bt1/HelloApplication.java
@@ -0,0 +1,23 @@
+package com.example.java1bt1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Java1-BT1.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Bai tap 1!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
